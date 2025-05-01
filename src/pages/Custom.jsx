1aa1cef22aaf7007347b6261342e77cfae8c9dd8
Amber <amberzeng@gmail.com>
2025-05-01 13:20:36 -0400

    custom testing

--- src/pages/Custom.jsx
+++ src/pages/Custom.jsx
@@ -19,7 +19,7 @@ function Custom() {
   };
 
   const hatMap = {
-    "top-hat": "/download-8.jpg",
+    topHat: "/download-8.jpg",
     cowboy: "/download-7.jpg",
     party: "/download-6.jpg",
   };
@@ -71,7 +71,7 @@ function Custom() {
             style={{ width: '100%', padding: '10px', borderRadius: '6px' }}
           >
             <option value="none">None</option>
-            <option value="top-hat">Top Hat</option>
+            <option value="topHat">Top Hat</option>
             <option value="cowboy">Cowboy Hat</option>
             <option value="party">Party Hat</option>
           </select>
